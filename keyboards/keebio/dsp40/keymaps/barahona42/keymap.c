7d4484fdece1cbe298863fde05989d8ca78b5f7a
Rene Barahona <rene.barahona@pnmac.com>
2023-12-17 16:36:21 -0800

    dsp40 updates

--- keyboards/keebio/dsp40/keymaps/barahona42/keymap.c
+++ keyboards/keebio/dsp40/keymaps/barahona42/keymap.c
@@ -21,8 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "process_combo.h"
 #include "encoder.h"
 
-#define LCTL_LGUI(keycode)SS_DOWN(X_LCTL)SS_DOWN(X_LGUI)SS_TAP(keycode)SS_UP(X_LCTL)SS_UP(X_LGUI)
-#define LCTL_LALT(keycode)SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_TAP(keycode)SS_UP(X_LCTL)SS_UP(X_LALT)
+#define LCTL_LGUI(keycode) SS_DOWN(X_LCTL)SS_DOWN(X_LGUI)SS_TAP(keycode)SS_UP(X_LCTL)SS_UP(X_LGUI)
+#define LCTL_LALT(keycode) SS_DOWN(X_LCTL)SS_DOWN(X_LALT)SS_TAP(keycode)SS_UP(X_LCTL)SS_UP(X_LALT)
+#define LCTL_LGUI_LALT_LSFT(keycode) SS_DOWN(X_LCTL)SS_DOWN(X_LGUI)SS_DOWN(X_LALT)SS_DOWN(X_LSFT)SS_TAP(keycode)SS_UP(X_LCTL)SS_UP(X_LGUI)SS_UP(X_LALT)SS_UP(X_LSFT)
 
 enum custom_keycodes {
     CGUIL = SAFE_RANGE,
@@ -41,6 +42,8 @@ enum custom_keycodes {
     CGUI0,
     CACOM,
     CADOT,
+    CCGASFTU,
+    CCGASFTD,
     CK_RAISE,
     CK_LOWER,
 };
@@ -165,8 +168,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_RAISE] = LAYOUT_ortho_4x12(
         BL_TOGG,     KC_HOME,    KC_UP,      KC_END,      KC_PGUP,     _______,   _______,     _______,     KC_LPRN,       KC_RPRN,       KC_EQL,    KC_DEL,
-        KC_GRV,      KC_LEFT,    KC_DOWN,    KC_RIGHT,    KC_PGDN,     _______,   _______,     _______,     KC_LBRC,       KC_RBRC,       _______,    _______,
-        _______,     KC_F12,     CACOM,      CADOT,       _______,     _______,   _______,     KC_MINS,     KC_LCBR,       KC_RCBR,       KC_BSLS,    _______,
+        KC_GRV,      KC_LEFT,    KC_DOWN,    KC_RIGHT,    KC_PGDN,     _______,   _______,     _______,     KC_LBRC,       KC_RBRC,       _______,    KC_BSLS,
+        _______,     KC_F12,     CACOM,      CADOT,       _______,     _______,   _______,     KC_MINS,     KC_LCBR,       KC_RCBR,       _______,    _______,
         _______,     _______,    _______,    MO(_FN1),    _______,     _______,   _______,     _______,     _______,       _______,    _______,    _______
     ),
     [_FN1] = LAYOUT_ortho_4x12(
@@ -185,10 +188,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_LOWER] =  { ENCODER_CCW_CW(KC_WH_U, KC_WH_D) },
-    [_RAISE] =  { ENCODER_CCW_CW(BL_DOWN, BL_UP) },
-    [_FN1] = { ENCODER_CCW_CW(CGUIL, CGUIR) },
+    [_LOWER] =  { ENCODER_CCW_CW(_______, _______) },
+    [_RAISE] =  { ENCODER_CCW_CW(_______, _______) },
+    [_FN1] = { ENCODER_CCW_CW(BL_DOWN, BL_UP) },
     [_NUMPAD] = { ENCODER_CCW_CW(_______, _______) }
 };
-
-

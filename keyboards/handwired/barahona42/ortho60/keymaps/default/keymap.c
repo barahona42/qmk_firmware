f78179ad81ff2aaf0db13494d0400fbf13c3a527
Rene Barahona <rene.barahona42@gmail.com>
2024-01-20 00:39:22 -0800

    updates

--- keyboards/handwired/barahona42/ortho60/keymaps/default/keymap.c
+++ keyboards/handwired/barahona42/ortho60/keymaps/default/keymap.c
@@ -142,8 +142,8 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_5x12(
         KC_GRV,      KC_1,       KC_2,       KC_3,        KC_4,        KC_5,      KC_6,        KC_7,        KC_8,          KC_9,       KC_0,       KC_MINS,
-        KC_ESC,      KC_Q,       KC_W,       KC_E,        KC_R,        KC_T,      KC_Y,        KC_U,        KC_I,          KC_O,       KC_P,       KC_BSPC,
-        KC_TAB,      KC_A,       KC_S,       KC_D,        KC_F,        KC_G,      KC_H,        KC_J,        KC_K,          KC_L,       KC_SCLN,    KC_QUOT,
+        KC_TAB,      KC_Q,       KC_W,       KC_E,        KC_R,        KC_T,      KC_Y,        KC_U,        KC_I,          KC_O,       KC_P,       KC_BSPC,
+        KC_ESC,      KC_A,       KC_S,       KC_D,        KC_F,        KC_G,      KC_H,        KC_J,        KC_K,          KC_L,       KC_SCLN,    KC_QUOT,
         TD(TD_LCPS), KC_Z,       KC_X,       KC_C,        KC_V,        KC_B,      KC_N,        KC_M,        KC_COMM,       KC_DOT,     KC_SLSH,    TD(TD_RCPS),
         KC_LCTL,     KC_LALT,    KC_LGUI,    MO(_LOWER),  KC_SPC,      KC_SPC,                 KC_SPC,      MO(_RAISE),    KC_RALT,    KC_RCTL,    KC_ENT
     ),
@@ -158,7 +158,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,     _______,    _______,    _______,     _______,     _______,   _______,     _______,     _______,       _______,    _______,    _______,
         _______,     KC_HOME,    KC_UP,      KC_END,      KC_PGUP,     _______,   _______,     _______,     KC_LPRN,       KC_RPRN,    KC_EQL,     KC_DEL,
         _______,     KC_LEFT,    KC_DOWN,    KC_RIGHT,    KC_PGDN,     _______,   _______,     KC_UNDS,     KC_LBRC,       KC_RBRC,    _______,    KC_BSLS,
-        _______,     KC_F12,     CACOM,      CADOT,       _______,     _______,   _______,     KC_MINS,     KC_LCBR,       KC_RCBR,    _______,    _______,
+        _______,     KC_F12,     CACOM,      CADOT,       _______,  -   _______,   _______,    KC_MINS,     KC_LCBR,       KC_RCBR,    _______,    _______,
         _______,     _______,    _______,    MO(_FN1),    _______,     _______,                _______,     _______,       _______,    _______,    _______
     ),
     [_FN1] = LAYOUT_ortho_5x12(
--- keyboards/keebio/dsp40/keymaps/barahona42/keymap.c
+++ keyboards/keebio/dsp40/keymaps/barahona42/keymap.c
@@ -167,20 +167,20 @@ tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_4x12(
-        TD(TD_MDIA), KC_Q,       KC_W,       KC_E,        KC_R,        KC_T,      KC_Y,        KC_U,        KC_I,          KC_O,       KC_P,       KC_BSPC,
+        KC_ESC,      KC_Q,       KC_W,       KC_E,        KC_R,        KC_T,      KC_Y,        KC_U,        KC_I,          KC_O,       KC_P,       KC_BSPC,
         KC_TAB,      KC_A,       KC_S,       KC_D,        KC_F,        KC_G,      KC_H,        KC_J,        KC_K,          KC_L,       KC_SCLN,    KC_QUOT,
         TD(TD_LCPS), KC_Z,       KC_X,       KC_C,        KC_V,        KC_B,      KC_N,        KC_M,        KC_COMM,       KC_DOT,     KC_SLSH,    TD(TD_RCPS),
         KC_LCTL,     KC_LALT,    KC_LGUI,    MO(_LOWER),  KC_SPC,      _______,    _______,      KC_SPC,      MO(_RAISE),    KC_RALT,    KC_RCTL,    KC_ENT
     ),
     [_LOWER] = LAYOUT_ortho_4x12(
-        _______,     KC_1,       KC_2,       KC_3,        KC_4,        KC_5,      KC_6,        KC_7,        KC_8,          KC_9,       KC_0,       _______,
-        KC_ESC,      KC_EXLM,    KC_AT,      KC_HASH,     KC_DLR,      KC_PERC,   KC_CIRC,     KC_AMPR,     KC_ASTR,       KC_LPRN,    KC_RPRN,    _______,
+        KC_GRV,      KC_1,       KC_2,       KC_3,        KC_4,        KC_5,      KC_6,        KC_7,        KC_8,          KC_9,       KC_0,       _______,
+        _______,     KC_EXLM,    KC_AT,      KC_HASH,     KC_DLR,      KC_PERC,   KC_CIRC,     KC_AMPR,     KC_ASTR,       KC_LPRN,    KC_RPRN,    _______,
         _______,     _______,    _______,    _______,     _______,     _______,    _______,    _______,     _______,       _______,    _______,    _______,
         TG(_NUMPAD), _______,    _______,    _______,     _______,     _______,   _______,     _______,     MO(_FN1),      _______,    _______,    _______
     ),
     [_RAISE] = LAYOUT_ortho_4x12(
         BL_TOGG,     KC_HOME,    KC_UP,      KC_END,      KC_PGUP,     _______,   _______,     _______,     KC_LPRN,       KC_RPRN,       KC_EQL,    KC_DEL,
-        KC_GRV,      KC_LEFT,    KC_DOWN,    KC_RIGHT,    KC_PGDN,     _______,   _______,     KC_UNDS,     KC_LBRC,       KC_RBRC,       _______,   KC_BSLS,
+        _______,     KC_LEFT,    KC_DOWN,    KC_RIGHT,    KC_PGDN,     _______,   _______,     KC_UNDS,     KC_LBRC,       KC_RBRC,       _______,   KC_BSLS,
         _______,     KC_F12,     CACOM,      CADOT,       _______,     _______,   _______,     KC_MINS,     KC_LCBR,       KC_RCBR,       _______,    _______,
         _______,     _______,    _______,    MO(_FN1),    _______,     _______,   _______,     _______,     _______,       _______,    _______,    _______
     ),

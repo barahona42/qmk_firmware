1c2bb5933db28eb553c1566d1f23029d8917b807
Rene Barahona <rene.barahona42@gmail.com>
2024-03-13 00:59:40 -0700

    equals kit: add rgb control

--- keyboards/boardsource/equals/avr/keymaps/barahona42/keymap.c
+++ keyboards/boardsource/equals/avr/keymaps/barahona42/keymap.c
@@ -143,15 +143,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,      KC_Q,       KC_W,       KC_E,        KC_R,        KC_T,      KC_Y,        KC_U,        KC_I,          KC_O,       KC_P,       KC_BSPC,
         KC_ESC,      KC_A,       KC_S,       KC_D,        KC_F,        KC_G,      KC_H,        KC_J,        KC_K,          KC_L,       KC_SCLN,    KC_QUOT,
         TD(TD_LCPS), KC_Z,       KC_X,       KC_C,        KC_V,        KC_B,      KC_N,        KC_M,        KC_COMM,       KC_DOT,     KC_SLSH,    TD(TD_RCPS),
-        _______, KC_LCTL,    KC_LALT,    KC_LGUI,     MO(_LOWER),  KC_SPC,    KC_SPC,      MO(_RAISE),  KC_RGUI,       KC_RALT,    KC_RCTL,       KC_ENT
+        _______,     KC_LCTL,    KC_LALT,    KC_LGUI,     MO(_LOWER),  KC_SPC,    KC_SPC,      MO(_RAISE),  KC_RGUI,       KC_RALT,    KC_RCTL,       KC_ENT
 
     ),
     [_LOWER] = LAYOUT_ortho_5x12(
         KC_F1,       KC_F2,      KC_F3,      KC_F4,       KC_F5,       KC_F6,     KC_F7,       KC_F8,       KC_F9,         KC_F10,     KC_F11,     KC_F12,
         _______,     _______,    _______,    _______,     _______,     _______,   _______,     _______,     _______,       _______,    _______,    _______,
         _______,     KC_EXLM,    KC_AT,      KC_HASH,     KC_DLR,      _______,   _______,     _______,     _______,       _______,    _______,    _______,
-        _______,     KC_PERC,    KC_CIRC,    KC_AMPR,     KC_ASTR,     _______,   _______,     _______,     _______,       _______,    _______,    _______,
-        _______,     _______,    _______,    _______,     _______,     _______,   _______,     MO(_FN1),     _______,      _______,    _______,    _______
+        _______,     KC_PERC,    KC_CIRC,    KC_AMPR,     KC_ASTR,     _______,   _______,     _______,     _______,       _______,    _______,    RGB_MOD,
+        _______,     _______,    _______,    _______,     _______,     _______,   _______,     MO(_FN1),     _______,      _______,    CK_TOGG,    RGB_TOG
     ),
     [_RAISE] = LAYOUT_ortho_5x12(
         _______,     _______,    _______,    _______,     _______,     _______,   _______,     _______,     _______,       _______,    _______,    _______,

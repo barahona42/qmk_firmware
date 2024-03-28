4aef4ddf601b30ac7fb743724f639a86675346c4
Rene Barahona <rene.barahona42@gmail.com>
2024-03-28 00:29:13 -0700

    updates keymap

--- keyboards/boardsource/equals/avr/keymaps/barahona42/keymap.c
+++ keyboards/boardsource/equals/avr/keymaps/barahona42/keymap.c
@@ -77,11 +77,11 @@ tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_5x12(
-        KC_GRV,      KC_1,       KC_2,       KC_3,        KC_4,        KC_5,      KC_6,        KC_7,        KC_8,          KC_9,       KC_0,       KC_CAPS,
+        KC_GRV,      KC_1,       KC_2,       KC_3,        KC_4,        KC_5,      KC_6,        KC_7,        KC_8,          KC_9,       KC_0,       _______,
         KC_TAB,      KC_Q,       KC_W,       KC_E,        KC_R,        KC_T,      KC_Y,        KC_U,        KC_I,          KC_O,       KC_P,       KC_BSPC,
         KC_ESC,      KC_A,       KC_S,       KC_D,        KC_F,        KC_G,      KC_H,        KC_J,        KC_K,          KC_L,       KC_SCLN,    KC_QUOT,
         KC_LSFT,     KC_Z,       KC_X,       KC_C,        KC_V,        KC_B,      KC_N,        KC_M,        KC_COMM,       KC_DOT,     KC_SLSH,    KC_RSFT,
-        _______,     KC_LCTL,    KC_LALT,    KC_LGUI,     MO(_LOWER),  KC_SPC,    KC_SPC,      MO(_RAISE),  KC_RGUI,       KC_RALT,    KC_RCTL,    KC_ENT
+        KC_CAPS,     KC_LCTL,    KC_LALT,    KC_LGUI,     MO(_LOWER),  KC_SPC,    KC_SPC,      MO(_RAISE),  KC_RGUI,       KC_RALT,    KC_RCTL,    KC_ENT
 
     ),
     [_LOWER] = LAYOUT_ortho_5x12(

32b56eb01f0cc2784d5cc206b80f1823b38dab8d
Rene Barahona <rene.barahona42@gmail.com>
2024-08-31 00:21:09 -0700

    atreus62: adds keymap

---
+++ keyboards/atreus62/keymaps/barahona42/keymap.c
@@ -0,0 +1,122 @@
+// this is the style you want to emulate.
+// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+
+#include QMK_KEYBOARD_H
+#include "keycodes.h"
+#include "process_combo.h"
+#include "customs.h"
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+const uint16_t PROGMEM combo_rprn_eql[] = {KC_RPRN, KC_EQL, COMBO_END};
+const uint16_t PROGMEM combo_eql_del[]   = {KC_EQL, KC_DEL, COMBO_END};
+const uint16_t PROGMEM combo_lbrc_rbrc[] = {KC_LBRC, KC_RBRC, COMBO_END};
+const uint16_t PROGMEM combo_rbrc_bsls[] = {KC_RBRC, KC_BSLS, COMBO_END};
+
+combo_t key_combos[] = {COMBO(combo_eql_del, KC_F12), COMBO(combo_rprn_eql, S(KC_F12)), COMBO(combo_lbrc_rbrc, LCTL(LALT(KC_COMM))), COMBO(combo_rbrc_bsls, LCTL(LALT(KC_DOT)))};
+
+bool handle_tap(keyrecord_t *record, char *keycode) {
+    if (record->event.pressed) {
+        send_string(keycode);
+    }
+    return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case CGUIL:
+            return handle_tap(record, LCTL_LGUI(X_LEFT));
+        case CGUIR:
+            return handle_tap(record, LCTL_LGUI(X_RIGHT));
+        case CGUIU:
+            return handle_tap(record, LCTL_LGUI(X_UP));
+        case CGUID:
+            return handle_tap(record, LCTL_LGUI(X_DOWN));
+        case CK_CACOM:
+            return handle_tap(record, LCTL_LALT(X_COMM));
+        case CK_CADOT:
+            return handle_tap(record, LCTL_LALT(X_DOT));
+    }
+    return true;
+};
+
+void handle_td_media(tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:
+            register_code(KC_MPLY);
+            break;
+        case 2:
+            register_code(KC_MNXT);
+            break;
+        case 3:
+            register_code(KC_MPRV);
+            break;
+    }
+}
+
+void handle_td_media_reset(tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 1:
+            unregister_code(KC_MPLY);
+            break;
+        case 2:
+            unregister_code(KC_MNXT);
+            break;
+        case 3:
+            unregister_code(KC_MPRV);
+            break;
+    }
+}
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_MDIA] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, handle_td_media, handle_td_media_reset)
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BASE] = LAYOUT( /* qwerty */
+        KC_GRV,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                            KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           _______,
+        KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,                                            KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSPC,
+        KC_ESC,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                            KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_QUOT,
+        KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                            KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_RSFT,
+        _______,        KC_LCTL,        KC_LALT,        KC_LGUI,        MO(_LOWER),     KC_SPC,          TT(_NUMS),        MO(_FN1),     KC_SPC,         MO(_RAISE),     KC_RGUI,        KC_RALT,        KC_RCTL,        KC_ENT
+    ),
+    [_LOWER] = LAYOUT(
+        KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F6,                                           KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,         KC_F12,
+        _______,        _______,        _______,        _______,        _______,        _______,                                         _______,        _______,        _______,        _______,        _______,        _______,
+        _______,        KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         _______,                                         _______,        _______,        _______,        _______,        _______,        _______,
+        _______,        KC_PERC,        KC_CIRC,        KC_AMPR,        KC_ASTR,        _______,                                         _______,        _______,        _______,        _______,        _______,        _______,
+        KC_CAPS,        _______,        _______,        _______,        _______,        _______,         _______,        _______,        _______,        MO(_FN1),       _______,        _______,        _______,        _______
+    ),
+    [_RAISE] = LAYOUT(
+        _______,        _______,        _______,        _______,        _______,        _______,                                         _______,        _______,        _______,        _______,        _______,        _______,
+        _______,        KC_HOME,        KC_UP,          KC_END,         KC_PGUP,        _______,                                         _______,        _______,        KC_LPRN,        KC_RPRN,        KC_EQL,         KC_DEL,
+        _______,        KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_PGDN,        _______,                                         _______,        KC_UNDS,        KC_LBRC,        KC_RBRC,        _______,        KC_BSLS,
+        _______,        KC_F12,         CK_CACOM,       CK_CADOT,       _______,        _______,                                         _______,        KC_MINS,        KC_LCBR,        KC_RCBR,        _______,        _______,
+        TD(TD_MDIA),    _______,        _______,        _______,        MO(_FN1),       _______,        _______,        _______,         _______,        _______,        _______,        _______,        _______,        _______
+    ),
+    [_FN1] = LAYOUT(
+        _______,        _______,        _______,        _______,        _______,        _______,                                         _______,        _______,        _______,        _______,        _______,        _______,
+        _______,        _______,        CGUIU,          _______,        _______,        _______,                                         _______,        _______,        _______,        _______,        _______,        _______,
+        _______,        CGUIL,          CGUID,          CGUIR,          _______,        _______,                                         _______,        _______,        _______,        _______,        _______,        _______,
+        _______,        _______,        _______,        _______,        _______,        _______,                                         _______,        _______,        _______,        _______,        _______,        _______,
+        _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,         _______,        _______,        _______,        _______,        _______,        QK_BOOT
+    ),
+    [_NUMS] = LAYOUT(
+        _______,        _______,        _______,        _______,        _______,        _______,                                         _______,        KC_SLSH,        KC_ASTR,        _______,        _______,        _______,
+        _______,        _______,        _______,        _______,        LSG(KC_TAB),    LGUI(KC_TAB),                                    KC_7,           KC_8,           KC_9,           KC_MINS,        _______,        _______,
+        _______,        _______,        _______,        _______,        LSG(KC_GRV),    LGUI(KC_GRV),                                    KC_4,           KC_5,           KC_6,           KC_PLUS,        _______,        _______,
+        _______,        _______,        _______,        _______,        RCS(KC_TAB),    LCTL(KC_TAB),                                    KC_1,           KC_2,           KC_3,           _______,        _______,        _______,
+        _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,         KC_0,           KC_DOT,         _______,        KC_ENT,         _______,        _______
+    ),
+};
+
+
+// [_THBR] = LAYOUT(
+//     _______,        _______,        _______,        _______,        _______,        _______,                                         _______,        _______,        _______,        _______,        _______,        _______,
+//     _______,        _______,        _______,        _______,        _______,        _______,                                         _______,        _______,        _______,        _______,        _______,        _______,
+//     _______,        _______,        _______,        _______,        _______,        _______,                                         _______,        _______,        _______,        _______,        _______,        _______,
+//     _______,        _______,        _______,        _______,        _______,        _______,                                         _______,        _______,        _______,        _______,        _______,        _______,
+//     _______,        _______,        _______,        _______,        _______,        _______,        _______,        _______,         _______,        _______,        _______,        _______,        _______,        _______
+// ),
---
+++ keyboards/atreus62/keymaps/barahona42/rules.mk
@@ -0,0 +1,4 @@
+# This file intentionally left blank
+CONVERT_TO = promicro_rp2040
+TAP_DANCE_ENABLE = yes
+COMBO_ENABLE = yes

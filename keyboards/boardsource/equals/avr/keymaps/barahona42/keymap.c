e55799204dbd3d1f44d920caf235168b4e500483
Rene Barahona <rene.barahona42@gmail.com>
2024-09-11 21:59:56 -0700

    equals-avr updates

--- keyboards/boardsource/equals/avr/keymaps/barahona42/keymap.c
+++ keyboards/boardsource/equals/avr/keymaps/barahona42/keymap.c
@@ -77,7 +77,7 @@ tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_5x12(
-        KC_GRV,      KC_1,       KC_2,       KC_3,        KC_4,        KC_5,      KC_6,        KC_7,        KC_8,          KC_9,       KC_0,       _______,
+        KC_GRV,      KC_1,       KC_2,       KC_3,        KC_4,        KC_5,      KC_6,        KC_7,        KC_8,          KC_9,       KC_0,       MO(_FN2),
         KC_TAB,      KC_Q,       KC_W,       KC_E,        KC_R,        KC_T,      KC_Y,        KC_U,        KC_I,          KC_O,       KC_P,       KC_BSPC,
         KC_ESC,      KC_A,       KC_S,       KC_D,        KC_F,        KC_G,      KC_H,        KC_J,        KC_K,          KC_L,       KC_SCLN,    KC_QUOT,
         KC_LSFT,     KC_Z,       KC_X,       KC_C,        KC_V,        KC_B,      KC_N,        KC_M,        KC_COMM,       KC_DOT,     KC_SLSH,    KC_RSFT,
@@ -108,8 +108,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FN2] = LAYOUT_ortho_5x12(
         _______,     _______,    _______,    _______,     _______,     _______,   _______,     _______,     _______,       _______,    _______,    _______,
         _______,     _______,    KC_PGUP,    _______,     _______,     _______,   _______,     _______,     _______,       _______,    _______,    _______,
-        _______,     KC_HOME,    KC_PGDN,    KC_END,      _______,     _______,   _______,     _______,     _______,       _______,    _______,    _______,
-        _______,     KC_F12,     CK_CACOM,   CK_CADOT,    _______,     _______,   _______,     _______,     _______,       _______,    _______,    _______,
+        _______,     RCS(KC_TAB),KC_PGDN,    LCTL(KC_TAB),      _______,     _______,   _______,     _______,     _______,       _______,    _______,    _______,
+        _______,     KC_HOME,    _______,    KC_END,    _______,     _______,   _______,     _______,     _______,       _______,    _______,    _______,
         _______,     _______,    _______,    _______,     _______,     _______,   _______,     _______,     _______,       _______,    _______,    _______
     )
 };

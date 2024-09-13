a51a56839137a9f8b23d511049ddf756524735e7
Rene Barahona <rene.barahona42@gmail.com>
2024-09-13 14:40:49 -0700

    macro9: update rules

--- keyboards/barahona42/macro9/rules.mk
+++ keyboards/barahona42/macro9/rules.mk
@@ -4,5 +4,5 @@ ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 
-# LTO_ENABLE = no
-# RGBLIGHT_ENABLE = no
+LTO_ENABLE = yes
+# RGBLIGHT_ENABLE = no ## might be able to enable this if i figure out how to set the rgb count
